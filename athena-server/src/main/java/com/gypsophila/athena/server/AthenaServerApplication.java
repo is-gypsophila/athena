38ceb73d20f5f2da7095c6877989c6341ccfcf11
李晓双 <644968328@qq.com>
2021-03-24 21:09:26 +0800

    initialize

---
+++ athena-server/src/main/java/com/gypsophila/athena/server/AthenaServerApplication.java
@@ -0,0 +1,13 @@
+package com.gypsophila.athena.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaServerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaServerApplication.class, args);
+    }
+    
+}
---
+++ athena-server/src/test/java/com/gypsophila/athena/server/AthenaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gypsophila.athena.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaServerApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
