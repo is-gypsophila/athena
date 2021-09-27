f7afd47391f613500ef3dedd1825b3005bf14fd9
lixiaoshuang <644968328@qq.com>
2021-09-27 23:47:57 +0800

    add grpc jar

---
+++ athena-server/src/main/java/org/gypsophila/athena/server/remote/GrpcServer.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 Gypsophila open source organization.
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gypsophila.athena.server.remote;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+/**
+ * @author lixiaoshuang
+ */
+public class GrpcServer {
+    private int port = 50051;//grpc服务端口
+    private Server server;//grpc server
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final GrpcServer server = new GrpcServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private void start() throws IOException {
+        //指定grpc服务器端口、接口服务对象，启动grpc服务器
+        server = ServerBuilder.forPort(port)
+                .build().start();
+        System.out.println("service start...");
+        //添加停机逻辑
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                GrpcServer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+}
